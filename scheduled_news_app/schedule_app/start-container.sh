5406088ae2f757ce7c83ae1cdfe6821826778256
ShivTushal <160385317+ShivTushal@users.noreply.github.com>
2025-04-18 14:13:53 +0530

    start-container.sh

--- scheduled_news_app/schedule_app/start-container.sh
+++ scheduled_news_app/schedule_app/start-container.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "[$(date)] Attempting to start git_main_1..." >> /tmp/container_launch.log
-podman start git_main_1 >> /tmp/container_launch.log 2>&1
+
+# Use full path to podman and specify socket
+/usr/bin/podman --url unix:///run/podman/podman.sock start git_main_1 >> /tmp/container_launch.log 2>&1
